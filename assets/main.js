1cf8f5882e9c88e2deb78d2ff8e969d792d7e82a
Honassis Lopes <honassislopes@gmail.com>
2019-09-19 15:13:43 -0300

    versão inicial feita, logica de reiniciar, avisos e velha

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,6 @@
 //X = true, O = false
-var player = false;
+var player = true;
+var fimdejogo = false;
 //recebe os valores da velha
 var campos = document.getElementsByClassName("p");
 
@@ -7,47 +8,85 @@ function status_jogador(){
     //retorna o jogador atual, e já atualiza o próximo player
     var status;
     if(player){
+        document.getElementById("player").innerHTML = "VEZ DE : O";
         status = "X";
     }else{
+        document.getElementById("player").innerHTML = "VEZ DE : X";
         status = "O";
     }
     player = !player;
+   
     return status;
+}
+function verificar(x_o){
+    /*não consegui pensar numa lógica para capturar as retas e as 
+    diagonais automaticamente, me ajuda aqui filipe!*/
+
+    var possibilidade = [[0, 1, 2], [0, 3, 6], [0, 4, 8], 
+                        [1, 4, 7], [2, 5, 8], [3, 4, 5], 
+                        [6, 4, 2], [6, 7, 8]];
+  
+    //verifica se o jogador ganhou                        
+    for (var i = 0; i < possibilidade.length; i++) {
+        if (
+            campos[possibilidade  [i]    [0]].innerHTML == x_o &&
+            campos[possibilidade  [i]    [1]].innerHTML == x_o &&
+            campos[possibilidade  [i]    [2]].innerHTML == x_o) {
+          
+              avisar("JOGADOR "+x_o+" VENCEU!");
+                fimdejogo = true;
+            
+            break;
+        }
+    }
+    if(!fimdejogo){
+        //se todos os campos forem preenchidos e não teve fim de jogo, significa que deu velha
+        var velha = 0;
+        for (var i = 0; i < campos.length; i++) {        
+                if (campos[i].innerHTML != "") {
+                    velha++;
+                }                                   
+        }
+        if (velha == campos.length) {
+            avisar("DEU VELHA!, CLIQUE EM REINICIAR");
+            fimdejogo = true;
+        }
+       
+    }
+}
+function avisar(mensagem){
+    document.getElementById("mensagem").innerHTML = mensagem;
+}
+function marcar(pos){
+    if(!fimdejogo){
+        var jogador = status_jogador();
+        //primeiro verifica se alguém não clicou, e então aplica
+        if(campos[pos].innerHTML==""){
+        campos[pos].innerHTML= jogador;
+        verificar(jogador);
+        }
+    }else{
+        avisar("JOGO JÁ FINALIZADO!, CLIQUE EM REINICIAR");
+    }
 }
 function carregar_clicks(i){
     campos[i].onclick= function() { marcar(i); };
 }
 
-function verificar(x_o){
-    var x = [0, 1, 2, 0, 3, 6, 0, 4, 8, 1, 4, 7, 2, 5, 8, 3, 4, 5, 4, 2, 6, 6, 7, 8];
-    var aux = 1; //essa variavel q é para dar uma ajuda nos saltos de 3 em 3 posisoes
-    //for para verificar se ganhou
-    for (var i = 0; i < x.length; i++) {
-        aux += 2;
-        //esses q-1,q-2,q-3, é pra justamente pegar os valores de 3 em 3
-        if (
-            campos[x[i + aux - 1]].innerHTML == x_o &&
-            campos[x[i + aux - 2]].innerHTML == x_o &&
-            campos[x[i + aux - 3]].innerHTML == x_o) {
-           alert(x_o+"--GANHOU");
-            
-            break;//para o for
-        }
-    }
-}
-
-function marcar(pos){
-    var jogador = status_jogador();
-  //primeiro verifica se alguém não clicou, e então aplica
-  if(campos[pos].innerHTML=="-"){
-    campos[pos].innerHTML= jogador;
-  }
-  verificar(jogador);
-}
 function iniciar(){
     for(var i = 0; i < campos.length;i++){
         //carregar todos os blocos de click
           carregar_clicks(i);
     }
+}
+function reiniciar(){
+    for(var i = 0; i < campos.length;i++){
+        //reseta os valores
+        campos[i].innerHTML= "";
+    }
+    fimdejogo = false;
+    avisar("");
+    player = true;
+    document.getElementById("player").innerHTML = "VEZ DE : X";
 }
 iniciar();
